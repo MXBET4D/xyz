b9c9daafdf8fa57f57ed1f20c5073d94aede7823
MXBET4D <vysor1999@gmail.com>
2025-10-02 21:38:53 +0700

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Ganti dengan konfigurasi Firebase kamu
+const firebaseConfig = {
+  apiKey: "AIzaSyDoerH0GZNG9QmCMCRqtmlFBbldKdCDIqw",
+  authDomain: "maxbet4d-b8bf0.firebaseapp.com",
+  databaseURL: "https://maxbet4d-b8bf0-default-rtdb.firebaseio.com",
+  projectId: "maxbet4d-b8bf0",
+  storageBucket: "maxbet4d-b8bf0.firebasestorage.app",
+  messagingSenderId: "1010739873755",
+  appId: "1:1010739873755:web:f63788eafc8237add389fe"
+};
+
+firebase.initializeApp(firebaseConfig);
